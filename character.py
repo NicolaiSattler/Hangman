eb19ca66b15750fc395f9bdd7bfbb88098bade2e
Nicolai Sättler <n.sattler@dataquint.nl>
2020-03-09 09:39:08 +0100

    master | added: max moves. changed: user can end game and restart is.

--- character.py
+++ character.py
@@ -1,6 +1,13 @@
 class Character:
     def __init__(self, char: str, index: int):
-        self.character = char;
-        self.indexes = []
-        self.indexes.append(index)
-
+        self._character = char.lower()
+        self._hidden = True
+        self._indexes = []
+        self._indexes.append(index)
+    
+    @property
+    def character(self):
+        return self._character
+    
+    @character.setter
+    def character(self, value):
--- hangman.py
+++ hangman.py
@@ -1,6 +1,5 @@
 import os
 from getpass import getpass
-import keyboard
 
 from character import Character
 
@@ -11,41 +10,97 @@ class Hangman:
         self.enter_word_text = "Word of choice:"
         self.enter_word_error_text = "No word was supplied"
         self.enter_character_text = "Start guessing...\n\n"
-        self.characters = []
-        self.characterCount = 0
-    
-    def SubscribeKeyboard(self):
-        keyboard.on_press(self.onKeyPress, False)
-            
-    def onKeyPress(self, name):
-        print(name)
-        
+        self.invalid_input = "Invalid input...\n"
+        self.victory_text = "You have awnsered correctly!\n\nStart a new game? Yes (Y) or No (N)? "
+        self.characterList = []
+        self.lettersAttempted = 0
+        self.maxAttempts = 10
+        self.letterCount = 0
+        self.awnser = ''
+        self.invalidInput = False
+       
     def StartGame(self):
         print(self.welcome_text)
 
-        result = self.UserSuppliesWord()
-
-        self.characterCount = len(result)
-        self.CreateResultCharacters(result)
+        self.awnser = self.UserSuppliesWord()
+        self.letterCount = len(self.awnser)
+        self.CreateResultCharacters(self.awnser)
         self.ClearText()
-        self.SubscribeKeyboard()
-        self.WriteAwnser()
-    
-    def WriteAwnser(self):
-        wordLength = ''
-        for i in range(0, self.characterCount):
-            wordLength+="_ "
-        
-        print(wordLength + "\n\n")
+        self.WriteInputInstructions()
+
+    def EndGame(self):
+        self.invalidInput = False
+        self.ClearText()
+
+        result = input(self.victory_text)
+
+        if result.upper() == 'Y':
+            self.ClearText()
+            self.StartGame()
+        elif result.upper() == 'N':
+            return
+        else:
+            self.invalidInput = True
+
+            print("Invalid Input..")
+            self.EndGame()
 
     def ClearText(self):
         os.system('cls' if os.name == 'nt' else "printf '\033c'")
+
+    def IncreaseAttemptCount(self):
+        self.lettersAttempted  = self.lettersAttempted + 1
+
+    def WriteInputInstructions(self):
+        self.IncreaseAttemptCount()
+        self.ClearText()
+        self.WriteAwnser()
+
+        result = input(f'Attempt {self.lettersAttempted} of {self.maxAttempts}:')
+        length = len(result)
+
+        if length == 0:
+            print(self.invalid_input)
+        elif length == 1:
+            self.onKeyPress(result)
+        elif length > 1:
+            if result.lower() == self.awnser:
+                self.EndGame()
+            else:
+                self.IncreaseAttemptCount()
+                print("Wrong awnser, pentaly point...\n")
+
+        self.WriteInputInstructions()
+
+    def WriteAwnser(self):
+        awnser = ''
+
+        for i in range(0, self.letterCount):
+            found = False
+
+            for item in self.characterList:
+                if not item.hidden and i in item.indexes:
+                    awnser+=f'{item.character} '
+                    found = True
+                    break
+
+            if not found:
+               awnser+="_ "
+
+        print(awnser + '\n')
+
+    def onKeyPress(self, letter: str):
+        for item in self.characterList:
+            if item.character == letter.lower():
+                item.hidden = False
+                break
+     
 
     def UserSuppliesWord(self):
         result = getpass(self.enter_word_text)
         
         if result:
-            return result;
+            return result.lower();
         else:
             print(self.enter_word_error_text)
 
@@ -55,13 +110,13 @@ class Hangman:
         item = self.GetCharacter(char)
         if item is None:
             item = Character(char, index)
-            self.characters.append(item)
+            self.characterList.append(item)
         else:
             item.indexes.append(index)
             
     def GetCharacter(self, char: str):
-        for item in self.characters:
-            if item.character is char:
+        for item in self.characterList:
+            if item.character == char:
                 return item
                 
     def CreateResultCharacters(self, hangmanAnwser: str):
@@ -69,5 +124,3 @@ class Hangman:
         for char in hangmanAnwser:
             self.AddCharacter(char, index)
             index = index + 1
-
-
